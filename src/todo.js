9ca5b46428b9f4c55d255f462fea2984104ac60d
Manish_Syal <manishsyal77@gmail.com>
2022-11-12 19:40:30 +0530

    user input is stored and div of that is put into loop and done coding for Delete buttton

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 
 const Todo = () => {
+  const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  //creating the additem function
+  const addItem = () => {
+    if(!inputdata){
+      alert("The input field is empty...! Plz fill the data")
+    }else{
+      const myNewInputData ={
+        id: new Date().getTime().toString(),
+        name: inputdata,
+      };
+      setItems([...items, myNewInputData])  //storing all data of user into an array after clicking on Plus(+) symbol
+      setInputData("");   //empty the input field of clicking on Plus(+) symbol
+    }
+  };
+
+  // how to delete items section
+  const deleteItem = (index) => {
+    const updatedItem = items.filter((curelem) => {
+      return curelem.id!==index; //returining those element those are not equal to the index(id) which we are getting as parameter when user click on delit icon.
+    });
+    setItems(updatedItem); //updatedItem will return an array of data excluding the object of deleted id
+  };
+
   return (
     <>
         <div className="main-div">
@@ -11,18 +36,28 @@ const Todo = () => {
                     <figcaption>Add Your List Here ✌️</figcaption>
                 </figure>
                 <div className="addItems">
-                    <input type="text" placeholder="✍️ Add Item" className="form-control"/>
-                    <i className="fa fa-plus add-btn"></i>
+                    <input 
+                    type="text"
+                     placeholder="✍️ Add Item"
+                      className="form-control"
+                      value={inputdata}
+                      onChange={(event) => setInputData(event.target.value)}
+                      />
+                    <i className="fa fa-plus add-btn" onClick={addItem}></i>
                 </div>
                       {/* show all items */}
                       <div className="showItems">
-                        <div className="eachItem">
-                          <h3>apple</h3>
-                          <div className="todo-btn">
-                          <i className="far fa-edit add-btn"></i>
-                          <i className="far fa-trash-alt add-btn"></i>
+                        {items.map((currElem) => {
+                          return (
+                            <div className="eachItem" key={currElem.id}>
+                               <h3>{currElem.name}</h3>
+                               <div className="todo-btn">
+                               <i className="far fa-edit add-btn"></i>
+                               <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(currElem.id)}></i>
                           </div>
                         </div>
+                          );
+                        })}
                       </div>
                  {/* remove all buttons */}
                 <div className="showItems">
