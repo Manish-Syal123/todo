4cb5a80580e9db67daa301d339a8c4761edcdb2e
Manish_Syal <manishsyal77@gmail.com>
2025-02-28 17:54:21 +0530

    Updated the todo.js file code to work with springBoot API's. To see the normal js code which was there before editing this file, just check the code till right previous commit.

--- src/todo.js
+++ src/todo.js
@@ -1,127 +1,132 @@
-import React, { useState, useEffect } from 'react'
-import "./style.css"
-
-//get the local storage data back 
-const getLocalData = () => {
-  const lists = localStorage.getItem("mytodolist");
-
-  if(lists) {
-    return JSON.parse(lists); //convert a string into array 
-  } else {
-    return [];
-  }
-};
-
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
 const Todo = () => {
-  const [inputdata, setInputData] = useState("");  //whatever the data user will add into the input field <input />
-  const [items, setItems] = useState(getLocalData());  //the Items having all the stored data of user n the form of array
-  const [isEditItem, setEditItem] = useState("");      //taking a id of that data to be edite
+  const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+  const [isEditItem, setEditItem] = useState(null);
   const [toggleButton, setToggleButton] = useState(false);
 
-  //creating the additem function
-  const addItem = () => {
-    if(!inputdata){
-      alert("The input field is empty...! Plz fill the data")
-    } else if (inputdata && toggleButton) {
-      setItems(
-        items.map((curElem) => {
-          if(curElem.id === isEditItem) {
-            return {...curElem, name: inputdata};
-          }
-          return curElem;
-        })
-      );
-
-      setInputData(""); //reset the input field to blank after updating
-      setEditItem(null);
-      setToggleButton(false);  //changing the update btn again to add(+) btn 
-    }
-    else{
-      const myNewInputData ={
-        id: new Date().getTime().toString(),
-        name: inputdata,
-      };
-      setItems([...items, myNewInputData])  //storing all data of user into an array after clicking on Plus(+) symbol
-      setInputData("");   //empty the input field of clicking on Plus(+) symbol
+  // Fetch Data Function
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/getAllItems");
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
   };
 
-  //edit the items
-  const editItem = (index) => {
-    const item_todo_edited = items.find((currElem) => {
-      return currElem.id === index;
-    });
-    setInputData(item_todo_edited.name);
-    setEditItem(index);
-    setToggleButton(true);
-  }
-
-  // how to delete items section
-  const deleteItem = (index) => {
-    const updatedItem = items.filter((curelem) => {
-      return curelem.id!==index; //returining those element those are not equal to the index(id) which we are getting as parameter when user click on delit icon.
-    });
-    setItems(updatedItem); //updatedItem will return an array of data excluding the object of deleted id
-  };
-
-  // remove all the elements
-  const removeAll = () => {
-    setItems([]);  
-  }
-
-  //adding localStorage
   useEffect(() => {
-    localStorage.setItem("mytodolist", JSON.stringify(items)); //as the localstorage() method takes parameter as key and value pair in String so thats why using Json.stringify() method we modified that items array into string 
-  }, [items]);
+    fetchData();
+  }, []);
+
+  const addItem = async () => {
+    if (!inputdata) {
+      alert("The input field is empty...! Plz fill the data");
+      return;
+    }
+
+    if (toggleButton) {
+      await fetch("http://localhost:8080/api/updateItem", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: isEditItem, name: inputdata }),
+      });
+      setToggleButton(false);
+    } else {
+      await fetch("http://localhost:8080/api/addItem", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: new Date().getTime().toString(),
+          name: inputdata,
+        }),
+      });
+    }
+
+    setInputData("");
+    setEditItem(null);
+    fetchData(); // Fetch updated list
+  };
+
+  const editItem = (id) => {
+    const itemToEdit = items.find((item) => item.id === id);
+    setInputData(itemToEdit.name);
+    setEditItem(id);
+    setToggleButton(true);
+  };
+
+  const deleteItem = async (id) => {
+    await fetch(`http://localhost:8080/api/deleteItem/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    fetchData(); // Fetch updated list
+  };
+
+  const removeAll = async () => {
+    await fetch("http://localhost:8080/api/items/deleteAllItems", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    fetchData(); // Fetch updated list
+  };
 
   return (
-    <>
-        <div className="main-div">
-            <div className="child-div">
-                <figure>
-                    <img src="./images/todo.svg" alt="todologo" />
-                    <figcaption>Add Your List Here ✌️</figcaption>
-                </figure>
-                <div className="addItems">
-                    <input 
-                    type="text"
-                     placeholder="✍️ Add Item"
-                      className="form-control"
-                      value={inputdata}
-                      onChange={(event) => setInputData(event.target.value)}
-                      />
-                    {toggleButton ? (
-                      <i className="far fa-edit add-btn" onClick={addItem}></i>
-                    ) : (
-                      <i className="fa fa-plus add-btn" onClick={addItem}></i>
-                    )}
-                </div>
-                      {/* show all items */}
-                      <div className="showItems">
-                        {items.map((currElem) => {
-                          return (
-                            <div className="eachItem" key={currElem.id}>
-                               <h3>{currElem.name}</h3>
-                               <div className="todo-btn">
-                               <i className="far fa-edit add-btn"
-                                    onClick={() => editItem(currElem.id)}></i> 
-                               <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(currElem.id)}></i>
-                          </div>
-                        </div>
-                          );
-                        })}
-                      </div>
-                 {/* remove all buttons */}
-                <div className="showItems">
-                  <button className="btn effect04"  data-sm-link-text="Remove All" onClick={removeAll}>
-                    <span>CHECK LIST</span>
-                    </button>
-                    </div>
+    <div className="main-div">
+      <div className="child-div">
+        <figure>
+          <img src="./images/todo.svg" alt="todologo" />
+          <figcaption>Add Your List Here ✌️</figcaption>
+        </figure>
+        <div className="addItems">
+          <input
+            type="text"
+            placeholder="✍️ Add Item"
+            className="form-control"
+            value={inputdata}
+            onChange={(event) => setInputData(event.target.value)}
+            onKeyDown={(event) => (event.key === "Enter" ? addItem() : null)}
+          />
+          {toggleButton ? (
+            <i className="far fa-edit add-btn" onClick={addItem}></i>
+          ) : (
+            <i className="fa fa-plus add-btn" onClick={addItem}></i>
+          )}
+        </div>
+        {/* show all items */}
+        <div className="showItems">
+          {items.map((currElem) => (
+            <div className="eachItem" key={currElem.id}>
+              <h3>{currElem.name}</h3>
+              <div className="todo-btn">
+                <i
+                  className="far fa-edit add-btn"
+                  onClick={() => editItem(currElem.id)}
+                ></i>
+                <i
+                  className="far fa-trash-alt add-btn"
+                  onClick={() => deleteItem(currElem.id)}
+                ></i>
+              </div>
             </div>
+          ))}
+        </div>
+        {/* remove all buttons */}
+        <div className="showItems">
+          <button
+            className="btn effect04"
+            data-sm-link-text="Remove All"
+            onClick={removeAll}
+          >
+            <span>CHECK LIST</span>
+          </button>
+        </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
